6e0b244de9fe86b5082611dffd9d876a47314cf4
Duy Tran <duytrankc1@gmail.com>
2020-05-29 23:17:44 +0700

    first commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab4.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            lab4Entities db = new lab4Entities();
+           ViewBag.data = db.Product.ToList();
+            return View();
+        }
+    }
+}
